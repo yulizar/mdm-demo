611a2e14cb11c04a51d7db1f5eec75d2081d3941
yulizar <erwinyulizar14@gmail.com>
2025-04-08 17:49:36 +0700

    [Update][MRP] auto split WO based on Work Center capacity

--- mrp_inherit/models/mrp_inherit.py
+++ mrp_inherit/models/mrp_inherit.py
@@ -1,4 +1,7 @@
 from odoo import fields, models, api, _
+from datetime import timedelta
+
+from odoo.tools import float_round
 
 
 class MrpProduction(models.Model):
@@ -11,4 +14,55 @@ class MrpProduction(models.Model):
             res.action_confirm()
             res.button_plan()
             res.action_start()
+        if res.workorder_ids:
+            for wo in res.workorder_ids:
+                wc_capacity = wo.workcenter_id.default_capacity
+                split_wo_count = wo.qty_remaining // wc_capacity
+                remainder_wo = wo.qty_remaining % wc_capacity
+                print(remainder_wo)
+                date_start = wo.date_start
+                date_finished = wo.date_finished
+                if remainder_wo > 0:
+                    for _ in range(int(split_wo_count)):
+                        date_start = date_start + timedelta(days=1)
+                        date_finished = date_finished + timedelta(days=1)
+                        new_wo = wo.copy()
+                        new_wo.update({
+                            'qty_remaining': wc_capacity,
+                            'date_start': date_start,
+                            'date_finished': date_finished,
+                        })
+
+                    wo.update({ 'qty_remaining':remainder_wo })
+                else:
+                    for _ in range(int(split_wo_count-1)):
+                        date_start = date_start + timedelta(days=1)
+                        date_finished = date_finished + timedelta(days=1)
+                        new_wo = wo.copy()
+                        new_wo.update({
+                            'qty_remaining': wc_capacity,
+                            'date_start': date_start,
+                            'date_finished': date_finished,
+                        })
+
+                    wo.update({'qty_remaining': wc_capacity})
+
         return res
+
+class MrpWorkorder(models.Model):
+    _inherit = 'mrp.workorder'
+
+    qty_remaining = fields.Float(store=True)
+
+    @api.depends('qty_production', 'qty_reported_from_previous_wo', 'qty_produced', 'production_id.product_uom_id')
+    def _compute_qty_remaining(self):
+        for wo in self:
+            if wo.production_id.product_uom_id:
+                if wo.qty_remaining > 0:
+                    continue
+                else:
+                    wo.qty_remaining = max(
+                    float_round(wo.qty_production - wo.qty_reported_from_previous_wo - wo.qty_produced,
+                                precision_rounding=wo.production_id.product_uom_id.rounding), 0)
+            else:
+                wo.qty_remaining = 0
